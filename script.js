ddf75333756e429489846977bc4619e9088cbbc2
Chandan Kumar <krc6893@gmail.com>
2022-12-10 13:45:18 +0530

    MultiImageSlider

---
+++ script.js
@@ -0,0 +1,37 @@
+const slide = document.querySelectorAll(".slide");
+prev = document.querySelector(".prev");
+next = document.querySelector(".next");
+
+slide.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+next.addEventListener("click", function () {
+  counter++;
+  // console.log(counter)
+  carousel();
+});
+
+prev.addEventListener("click", function () {
+  counter--;
+  // console.log(counter)
+  carousel();
+});
+setInterval(() => {
+  counter++;
+  carousel();
+}, 4000);
+function carousel() {
+  if (counter === slide.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slide.length - 1;
+  }
+
+  slide.forEach(function (item) {
+    item.style.transform = `translateX(-${counter * 100}%)`;
+    // console.log((item.style.transform = `translateX(${counter * 100}%)`));
+  });
+}
